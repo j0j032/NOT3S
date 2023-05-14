bfabd0652cf1da741b0ee40b29173a3967aeb885
Jordan Martinellli <jordan@pyramidanatomy.com>
2023-05-14 21:32:04 +0200

    init

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,7 +4,7 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -47,9 +47,11 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GithubProvider({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      clientId: env.GITHUB_CLIENT_ID,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
